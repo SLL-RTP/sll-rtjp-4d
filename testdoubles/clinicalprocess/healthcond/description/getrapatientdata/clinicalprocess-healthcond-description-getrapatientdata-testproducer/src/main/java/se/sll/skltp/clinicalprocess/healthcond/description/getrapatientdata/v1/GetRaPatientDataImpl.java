03c504d6c93754729b2334f3931b03c62d7b3776
Peter Merikan <peter.merikan@callistaenterprise.se>
2013-10-31 14:36:31 +0100

    added date and random drugs

--- testdoubles/clinicalprocess/healthcond/description/getrapatientdata/clinicalprocess-healthcond-description-getrapatientdata-testproducer/src/main/java/se/sll/skltp/clinicalprocess/healthcond/description/getrapatientdata/v1/GetRaPatientDataImpl.java
+++ testdoubles/clinicalprocess/healthcond/description/getrapatientdata/clinicalprocess-healthcond-description-getrapatientdata-testproducer/src/main/java/se/sll/skltp/clinicalprocess/healthcond/description/getrapatientdata/v1/GetRaPatientDataImpl.java
@@ -21,86 +21,131 @@
 package se.sll.skltp.clinicalprocess.healthcond.description.getrapatientdata.v1;
 
 import java.math.BigInteger;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
 import javax.jws.WebService;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import se.sll.clinicalprocess.healthcond.description.getrapatientdata.v1.rivtabp21.GetRaPatientDataResponderInterface;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.DoctorGlobalEnum;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.DoctorVariables;
+import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.DrugType;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.GetRaPatientDataResponseType;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.GetRaPatientDataType;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.LabVariables;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.PatientDataType;
+import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.PatientDataType.Drugs;
 import se.sll.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.PatientVariables;
 
 //@WebService(serviceName = "GetRaPatientDataResponderService", endpointInterface = "se.riv.clinicalprocess.healthcond.description.getrapatientdataresponder.v1.rivtabp21.GetRaPatientDataResponderInterface", portName = "GetRaPatientDataResponderPort", targetNamespace = "urn:sll:clinicalprocess:healthcond:description:GetRaPatientData:1:rivtabp21", wsdlLocation = "interactions/GetRaPatientDataInteraction/GetRaPatientDataInteraction_1.0_RIVTABP21.wsdl")
 @WebService
 public class GetRaPatientDataImpl implements GetRaPatientDataResponderInterface {
 
-	@Override
-	public GetRaPatientDataResponseType getRaPatientData(String arg0,
-			GetRaPatientDataType arg1) {
+    @Override
+    public GetRaPatientDataResponseType getRaPatientData(String arg0,
+            GetRaPatientDataType arg1) {
 
-		GetRaPatientDataResponseType response = new GetRaPatientDataResponseType();
-		List<PatientDataType> result = response.getPatientData();
-		StringBuffer comment = new StringBuffer("Test result for ");
+        GetRaPatientDataResponseType response = new GetRaPatientDataResponseType();
+        List<PatientDataType> result = response.getPatientData();
+        StringBuffer comment = new StringBuffer("Test result for ");
 
-		if (arg1 != null) {
-			comment.append("[" + arg1.getPatientId() + "]");
-			
-			int iter = randInt(1, 10);
-			for (int i = 0; i < iter; i++) {
-				result.add(produceRandomPatientData());				
-			}
-			
-		} 
-		response.setComment(comment.toString());
+        if (arg1 != null) {
+            comment.append("[" + arg1.getPatientId() + "]");
 
-		return response;
-	}
+            int iter = randInt(1, 10);
+            for (int i = 0; i < iter; i++) {
+                result.add(produceRandomPatientData());
+            }
 
-	private PatientDataType produceRandomPatientData() {
-		PatientDataType data = new PatientDataType();
-		PatientVariables p = new PatientVariables();
-		p.setWorkability(randBigInt(0, 5));
-		p.setGlobalHealth(randBigInt(0, 5));
-		p.setPain(randBigInt(0, 5));
-		p.setEq5D((double) randInt(7, 190)/10);
-		p.setHaq((double) randInt(7, 190)/10);
-		p.setTenderJoints28(randBigInt(0, 5));
-		p.setSwollenJoints28(randBigInt(0, 5));
-		data.setPatientVariables(p);
-		
-		DoctorVariables d = new DoctorVariables();
-		d.setDoctorGlobal(DoctorGlobalEnum.values()[randInt(0, DoctorGlobalEnum.values().length-1)]);
-		d.setTenderJoints28(randBigInt(0, 5));
-		d.setSwollenJoints28(randBigInt(0, 5));
-		d.setDAS28((float)randInt(7, 199)/10);
-		d.setDAS28CRP((float)randInt(7, 199)/10);
-		data.setDoctorsVariables(d);
-		
-		LabVariables l = new LabVariables();
-		l.setSR((float)randInt(7, 199));
-		l.setCRP((float)randInt(7, 199));
-		data.setLabVariables(l);
-		return data;
-	}
+        }
+        response.setComment(comment.toString());
 
-	private int randInt(int min, int max) {
+        return response;
+    }
 
-		// Usually this can be a field rather than a method variable
-		Random rand = new Random();
+    private PatientDataType produceRandomPatientData() {
+        PatientDataType data = new PatientDataType();
+        data.setDate("20131001");
+        PatientVariables p = new PatientVariables();
+        p.setWorkability(randBigInt(0, 5));
+        p.setGlobalHealth(randBigInt(0, 5));
+        p.setPain(randBigInt(0, 5));
+        p.setEq5D((double) randInt(7, 190) / 10);
+        p.setHaq((double) randInt(7, 190) / 10);
+        p.setTenderJoints28(randBigInt(0, 5));
+        p.setSwollenJoints28(randBigInt(0, 5));
+        data.setPatientVariables(p);
 
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		int randomNum = rand.nextInt((max - min) + 1) + min;
+        DoctorVariables d = new DoctorVariables();
+        d.setDoctorGlobal(DoctorGlobalEnum.values()[randInt(0,
+                DoctorGlobalEnum.values().length - 1)]);
+        d.setTenderJoints28(randBigInt(0, 5));
+        d.setSwollenJoints28(randBigInt(0, 5));
+        d.setDAS28((float) randInt(7, 199) / 10);
+        d.setDAS28CRP((float) randInt(7, 199) / 10);
+        data.setDoctorsVariables(d);
 
-		return randomNum;
-	}
-	private BigInteger randBigInt(int min, int max) {
-		return BigInteger.valueOf(randInt(min, max));
-	}
+        LabVariables l = new LabVariables();
+        l.setSR((float) randInt(7, 199));
+        l.setCRP((float) randInt(7, 199));
+        data.setLabVariables(l);
+
+        data.setDrugs(produceRandomDrugs());
+
+        return data;
+    }
+
+    private Drugs produceRandomDrugs() {
+        Drugs drugs = new Drugs();
+
+        List<DrugType> list = drugs.getDrug();
+        int iter = randInt(1, 5);
+        for (int i = 0; i < iter; i++) {
+            DrugType drug = new DrugType();
+            drug.setDose((float) randInt(7, 190) / 10);
+            drug.setName("name-" + i);
+            drug.setEndCause("endcause-" + i);
+            drug.setInterval(Integer.toString(i));
+            drug.setType("type-" + i);
+            drug.setStartDate(convertToXmlDate(new Date()));
+            drug.setEndDate(convertToXmlDate(new Date()));
+            list.add(drug);
+        }
+        return drugs;
+    }
+
+    private int randInt(int min, int max) {
+
+        // Usually this can be a field rather than a method variable
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+    private BigInteger randBigInt(int min, int max) {
+        return BigInteger.valueOf(randInt(min, max));
+    }
+
+    private XMLGregorianCalendar convertToXmlDate(Date myDate) {
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTime(myDate);
+        XMLGregorianCalendar date = null;
+        try {
+            date = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        } catch (DatatypeConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return date;
+    }
 
 }
